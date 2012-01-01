718222acf321ec5bc9363f332c34eb335491f1b4
Terrell Russell <terrellrussell@gmail.com>
2012-01-01 15:21:43 -0500

    update setup script filenames

--- setupmpact.sh
+++ setupmpact.sh
@@ -6,7 +6,8 @@ chmod 600 droptables.sql
 chmod 644 mpact_db.php
 chmod 600 mpact_db.php.sample
 chmod 700 setupmpact.sh
-chmod 700 cron.php
+chmod 700 cron-manual.php
+chmod 700 cron-regenerate-dirty.php
 echo "removing cached dotgraphs..."
 for a in 1 2 3 4 5 6 7 8 9
 do
