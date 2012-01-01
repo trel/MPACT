c89b1f996eb4f92d176852df4f0b4a37806d2c61
Terrell Russell <terrellrussell@gmail.com>
2012-01-01 17:33:15 -0500

    better logging

--- cron-regenerate-dirty.php
+++ cron-regenerate-dirty.php
@@ -10,10 +10,10 @@ while ( $line = mysql_fetch_array($result)) {
   array_push($dirtyrecords,$line['id']);
 }
 
-echo `date`." - found ".count($dirtyrecords)." records\n";
+echo date("Y-m-d\TH:i:s")." found ".count($dirtyrecords)." record(s)\n";
 # generate graphs and clean the records
 foreach ($dirtyrecords as $person){
-  print "- regenerating for [$person]...\n";
+  print "- regenerating for [$person]\n";
   generate_dotgraph($person,"force");
   $query = "UPDATE people SET regenerate_dotgraph = '0' WHERE id = '".$person."'";
   $result = mysql_query($query) or die(mysql_error());
